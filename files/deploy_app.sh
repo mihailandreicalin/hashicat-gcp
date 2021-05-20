ad11d37b5a9cf9d67d028bb2d361cc9ff99b5afe
Andrei Calin <macalin@gmail.com>
2021-05-20 16:05:20 +0000

    mewa

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Meaw Meaw Meaw.
+  Welcome to ${PREFIX}'s app. Catch this cat: Meaw Meaw Meaw.
   <!-- END -->
 
   </div>
