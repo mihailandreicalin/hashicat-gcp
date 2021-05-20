4d86574cfb3167018f564bd332083b426a8dc01b
Andrei Calin <macalin@gmail.com>
2021-05-20 17:25:56 +0000

    sdfds

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -3,6 +3,11 @@
 # The web app has a customizable image and some text.
 
 cat << EOM > /var/www/html/index.html
+<meta http-equiv="cache-control" content="max-age=0" />
+<meta http-equiv="cache-control" content="no-cache" />
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+<meta http-equiv="pragma" content="no-cache" />
 <html>
   <head><title>Meow!</title></head>
   <body>
